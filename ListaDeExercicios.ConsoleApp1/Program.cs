4a004e2487cb2a541daf73a299cd885043a5189e
Eduardoborges063 <1eduardomacedo2525@gmail.com>
2025-11-11 20:44:13 -0300

    Adicionar arquivos de projeto.

---
+++ ListaDeExercicios.ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class Exercicio1
+{
+    static void Main()
+    {
+        Console.Write("Digite o comprimento da caixa: ");
+        double comprimento = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite a largura da caixa: ");
+        double largura = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite a altura da caixa: ");
+        double altura = double.Parse(Console.ReadLine());
+
+        double volume = comprimento * largura * altura;
+        Console.WriteLine($"O volume da caixa é: {volume:F2}");
+    }
+}
+
+class Exercicio2
+{
+    static void Main()
+    {
+        Console.Write("Digite o raio do cilindro: ");
+        double raio = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite a altura do cilindro: ");
+        double altura = double.Parse(Console.ReadLine());
+
+        double volume = Math.PI * Math.Pow(raio, 2) * altura;
+        Console.WriteLine($"O volume do cilindro é: {volume:F2}");
+    }
+}
+class Exercicio3
+{
+    static void Main()
+    {
+        Console.Write("Digite a quilometragem inicial: ");
+        double kmInicial = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite a quilometragem final: ");
+        double kmFinal = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite o combustível consumido (litros): ");
+        double combustivel = double.Parse(Console.ReadLine());
+
+        double distancia = kmFinal - kmInicial;
+        double consumo = distancia / combustivel;
+
+        Console.WriteLine($"O consumo médio foi de {consumo:F2} km/L");
+    }
+}
+
